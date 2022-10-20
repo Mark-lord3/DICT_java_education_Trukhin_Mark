21d06f3a2e92267d969047f7bc8a968764d51a03
Mark-lord3 <m.s.trukhin@student.khai.edu>
2022-10-20 16:03:56 +0200

    Resolved conflict
    Test.java added to feature_branch with
    change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,13 @@
 public class Test {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java education");
         
+=======
+        System.out.println("education");
+        //new string
+        System.out.println("DICT");
+    }
+>>>>>>> 7f08626 (Test.java added to feature_branch with)
 }
